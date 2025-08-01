039fc2811b69877c417a943ebd20e9e2bf0f28e8
VedantRathor <92686119+VedantRathor@users.noreply.github.com>
2025-08-02 04:14:42 +0530

    adapter-design-with-problem initiated.

---
+++ LLDProblems/AdapterPatternWithProblem/Main.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.LLDProblems.AdapterPatternWithProblem;
+
+public class Main {
+    public static void main(String varchar[]) {
+        System.out.println("LLD for adapter pattern is running fine..");
+    }
+}
