26b1e37b03df03f1b609dd0a1f46e02df058622e
VedantRathor <92686119+VedantRathor@users.noreply.github.com>
2025-08-02 01:26:20 +0530

    file system initiation

---
+++ LLDProblems/FileSystem/FileSystem.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.LLDProblems.FileSystem;
+
+public class FileSystem {
+    public static void main(String varchars[]) {
+        System.out.println("LLD of File System is working...");
+    }
+}
