b20469161edf047346dfda527aff7f2938d517dc
VedantRathor <92686119+VedantRathor@users.noreply.github.com>
2025-08-02 21:12:25 +0530

    srp

--- LLDProblems/BuilderDesignPattern/Main.java
+++ LLDProblems/BuilderDesignPattern/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String varchar[]) {
         System.out.println("LLD for builder-design-pattern is running...");
         StudentBuilder engineeringStudentBuilder = new EngineeringStudentBuilder(1001);
-        Student engStudent = engineeringStudentBuilder.setMotherName("Sunila").setFatherName("Father").setName("Vedant Rathore")
+        Student engStudent = engineeringStudentBuilder
+                .setMotherName("XYZ")
+                .setFatherName("ABC")
+                .setName("Vedant Rathore")
                 .addSubject("DSA")
                 .addSubject("OS")
                 .addSubject("CP")
---
+++ SOLID/SingleResponsibilityPrinciple.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.SOLID;
+
+public class SingleResponsibilityPrinciple {
+    public static void main(String varchar[]) {
+        System.out.println("SRP..");
+    }
+}
