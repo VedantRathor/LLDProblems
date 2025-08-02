32e6b6c943cccce2f9edcfeee3f88367b63c1bf2
VedantRathor <92686119+VedantRathor@users.noreply.github.com>
2025-08-03 00:57:25 +0530

    design pizza with decorator

---
+++ DesignPatterns/DecoratorDP/DesignPizzaWithDecorator/Main.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.DesignPatterns.DecoratorDP.DesignPizzaWithDecorator;
+
+public class Main {
+    public static void main(String varchar[]) {
+        System.out.println("Design pizza with decorator running fine....");
+    }
+}
