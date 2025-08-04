7eadb77dce58e1febcff2a8d9ef7c8d7d84a5466
Vedant Rathore <vedantrathore627@gmail.com>
2025-08-04 15:50:29 +0530

    DB Schema for Tic-Tac-Toe

---
+++ LLDProblems/TicTacToe/DBSchema/DBSchema.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.LLDProblems.TicTacToe.DBSchema;
+
+public class DBSchema {
+    public static void main(String varchar[]) {
+        System.out.println("DBSchema running fine...");
+    }
+}
