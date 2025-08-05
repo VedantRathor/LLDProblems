c8adedb8207799abee46113ed7230b458af109d5
Vedant Rathore <vedant.rathore@HS5CG1416C3X.hs.local>
2025-08-05 10:40:14 +0530

    updates

--- LLDProblems/TicTacToe/DBSchema/DBSchema.java
+++ LLDProblems/TicTacToe/DBSchema/DBSchema.java
@@ -3,5 +3,6 @@ package com.cloudvendor.cloudvendor.LLDProblems.TicTacToe.DBSchema;
 public class DBSchema {
     public static void main(String varchar[]) {
         System.out.println("DBSchema running fine...");
+        System.out.println("DBSchema running fine...");
     }
 }
