1f20c85f909e2240eb9a6486289aa09e511e3cfd
Vedant Rathore <vedant.rathore@HS5CG1416C3X.hs.local>
2025-08-08 18:27:28 +0530

    updates

--- LLDProblems/BookMyShow/Main.java
+++ LLDProblems/BookMyShow/Main.java
@@ -2,7 +2,6 @@ package com.cloudvendor.cloudvendor.LLDProblems.BookMyShow;
 
 public class Main {
     public static void main(String varchars[]) {
-        System.out.println("Book my show is working fine...");
-        System.out.println("Book my show is working fine...");
+        System.out.println("book my show is running fine....");
     }
 }
