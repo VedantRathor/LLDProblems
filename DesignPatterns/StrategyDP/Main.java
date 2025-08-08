d0a48058c1992fd30a22b03335c80b282ced2159
Vedant Rathore <vedant.rathore@HS5CG1416C3X.hs.local>
2025-08-08 17:50:31 +0530

    updates

--- DesignPatterns/StrategyDP/Main.java
+++ DesignPatterns/StrategyDP/Main.java
@@ -1,7 +1,22 @@
 package com.cloudvendor.cloudvendor.DesignPatterns.StrategyDP;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String varchar[]) {
+        System.out.println("running from CLI");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("ENter you mobile number");
+        long mobileNumber = sc.nextLong();
 
+        System.out.println("1. Print; 2. Exit");
+        int press = sc.nextInt();
+
+        if (press == 1) {
+          System.out.println("Your name is: " + name);
+          System.out.println("Your mobile number is: " + mobileNumber);
+        }
     }
 }
