02f6057e5cd077ebf5433c6759c88e85f8fe3cb1
Vedant Rathore <vedant.rathore@HS5CG1416C3X.hs.local>
2025-08-11 16:09:48 +0530

    bitly and whatsapp HLD.

---
+++ HLDModule/Problems/Bit/ly/BitLy.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.HLDModule.Problems.Bit.ly;
+
+public class BitLy {
+    public static void main(String varargs[]) {
+        System.out.println("Bit ly is running fine....");
+    }
+}
---
+++ HLDModule/Problems/WhatsApp/WhatsApp.java
@@ -0,0 +1,7 @@
+package com.cloudvendor.cloudvendor.HLDModule.Problems.WhatsApp;
+
+public class WhatsApp {
+    public static void main(String varargs[]) {
+        System.out.println("whatsapp is running fine...");
+    }
+}
